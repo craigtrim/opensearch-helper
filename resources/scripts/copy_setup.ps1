812157f3e9ccab37f3d1cc9ea06585090bcd648c
Craig Trim <craigtrim@gmail.com>
2022-10-11 09:56:05 -0700

    Fix Defect in ScoreTopHit

--- resources/scripts/copy_setup.ps1
+++ resources/scripts/copy_setup.ps1
@@ -1,7 +1,7 @@
 # Full path of the file
 $file = 'setup.py'
 $ProjectName = "opensearch-helper"
-$ProjectVersion = "0.1.5    "
+$ProjectVersion = "0.1.6"
 
 # remove the file if it exists
 if (Test-Path -Path $file -PathType Leaf) {
